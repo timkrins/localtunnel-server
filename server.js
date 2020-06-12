8afb71481b8c2f508571f54de781c2129a447cb5
Tim Krins <timkrins@gmail.com>
2020-06-12 13:38:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import Koa from 'koa';
 import tldjs from 'tldjs';
 import Debug from 'debug';
 import http from 'http';
-import { hri } from 'human-readable-ids';
 import Router from 'koa-router';
 
 import ClientManager from './lib/ClientManager';
@@ -65,7 +64,7 @@ export default function(opt) {
 
         const isNewClientRequest = ctx.query['new'] !== undefined;
         if (isNewClientRequest) {
-            const reqId = hri.random();
+            const reqId = 'one';
             debug('making new client with id %s', reqId);
             const info = await manager.newClient(reqId);
 
